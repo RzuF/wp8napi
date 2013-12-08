956a5fe3c62ed00dbac8db543c113d06ef8f09a5
RzuF <rzuf22@gmail.com>
2013-12-08 17:11:08 +0100

    first

---
+++ wp8napiv2/Page1.xaml.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using System.Windows.Media;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace wp8napiv2
+{
+    public partial class Page1 : PhoneApplicationPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        private void Play()
+        {
+
+        }
+
+        private void setSubs()
+        {
+            //MessageBox.Show(PivotPage1.napisy);
+
+            //TimelineMarkerCollection marks =  new TimelineMarkerCollection();
+
+            string[] x = new string[1] { "\n" };
+            string[] lines = PivotPage1.napisy.Split(x, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (string i in lines)
+            {
+                TimelineMarker uno = new TimelineMarker();
+                TimelineMarker dos = new TimelineMarker();
+                // RegEx
+
+                //MessageBox.Show(i);
+
+                Regex reg = new Regex(@"\[\d*\]");
+                MatchCollection matches = reg.Matches(i);
+                if (matches.Count == 2)
+                {
+                    string time_p = matches[0].Value.Substring(1, matches[0].Value.Length - 2);
+                    string time_k = matches[1].Value.Substring(1, matches[1].Value.Length - 2);
+
+                    string text = i.Substring(time_p.Length + time_k.Length + 4);
+
+                    //MessageBox.Show("Tekst: " + text + "\n\nP: " + time_p + "\n\nK: " + time_k);
+
+                    uno.Text = text;
+                    dos.Text = "";
+
+                    //double time_p_d = Double.Parse(time_p) / 23.976;
+                    //double time_k_d = Double.Parse(time_k) / 23.976;
+                    float time_p_d = float.Parse(time_p) / 10;
+                    float time_k_d = float.Parse(time_k) / 10;
+                    int m_p = (int)(time_p_d / 60);
+                    time_p_d = time_p_d - 60 * m_p;
+                    int m_k = (int)(time_k_d / 60);
+                    time_k_d = time_k_d - 60 * m_k;
+                    int h_p = m_p / 60;
+                    m_p = m_p - 60 * h_p;
+                    int h_k = m_k / 60;
+                    m_k = m_k - 60 * h_k;
+
+                    string time_p_s = h_p.ToString() + ":" + m_p.ToString() + ":" + time_p_d.ToString();
+                    string time_k_s = h_k.ToString() + ":" + m_k.ToString() + ":" + time_k_d.ToString();
+
+                    //string last = lines.Last();
+
+                    //if(i==lines.Last()) { MessageBox.Show("P: " + time_p_s + "\n\nK: " + time_k_s + "\n\nLine: " + i); break;}
+
+                    try
+                    {
+                        uno.Time = TimeSpan.Parse(time_p_s);
+                        dos.Time = TimeSpan.Parse(time_k_s);
+                    }
+                    catch (Exception e) { MessageBox.Show(e.Message + "\n\nP: " + time_p_s + "\n\nK: " + time_k_s); }
+                    //uno.Type = "subs";
+                    //dos.Type = "subs";
+
+                    //marks.Add(uno);
+                    //marks.Add(dos);
+                    try
+                    {
+                        //MessageBox.Show("Przed dodaniem");
+
+                        myPlayer.Markers.Add(uno);
+                        myPlayer.Markers.Add(dos);
+
+                        //MessageBox.Show("Po dodaniu");
+                    }
+                    catch (Exception e) { MessageBox.Show(e.Message); }
+                }
+                else
+                {
+                    MessageBox.Show("Błedny format napisów. Liczba: " + matches.Count.ToString());
+
+                    string tmp = "";
+                    foreach (Match j in matches) tmp += j.Value + "\n";
+
+                    MessageBox.Show(tmp);
+
+                    break;
+                }
+
+            }
+
+            //MessageBox.Show("Po dodaniu wszystkich");
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            string patch;
+
+            NavigationContext.QueryString.TryGetValue("patch", out patch);
+
+            //MessageBox.Show("Przed filmem");
+
+            myPlayer.Source = new Uri(patch, UriKind.RelativeOrAbsolute);
+            //myPlayer.Pause();
+
+            //setSubs();
+        }
+
+        private void myPlayer_MarkerReached(object sender, TimelineMarkerRoutedEventArgs e)
+        {
+            subs.Text = e.Marker.Text.ToString().Replace("|", "\n");
+            //MessageBox.Show(e.Marker.Text);
+        }
+
+        private void myPlayer_MediaOpened(object sender, RoutedEventArgs e)
+        {
+            setSubs();
+
+            //MessageBox.Show("Opened");
+        }
+
+        private void myPlayer_Tap(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+            myPlayer.MarkerReached -= new TimelineMarkerRoutedEventHandler(myPlayer_MarkerReached);
+
+            if (myPlayer.CurrentState == MediaElementState.Playing) myPlayer.Pause();
+            if (myPlayer.CurrentState == MediaElementState.Paused || myPlayer.CurrentState == MediaElementState.Stopped) myPlayer.Play();
+
+            subs.Text = "";
+            //MessageBox.Show(myPlayer.Markers.Count.ToString());
+            //myPlayer.MarkerReached += new TimelineMarkerRoutedEventHandler(myPlayer_MarkerReached);
+        }
+
+        private void myPlayer_CurrentStateChanged(object sender, RoutedEventArgs e)
+        {
+            //myPlayer.MarkerReached -= new TimelineMarkerRoutedEventHandler(myPlayer_MarkerReached);
+            myPlayer.MarkerReached += new TimelineMarkerRoutedEventHandler(myPlayer_MarkerReached);
+        }
+
+        private void myPlayer_Hold(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+            MessageBox.Show(myPlayer.Markers.Count.ToString());
+            myPlayer.MarkerReached += new TimelineMarkerRoutedEventHandler(myPlayer_MarkerReached);
+        }
+    }
+}
---
+++ wp8napiv2/PivotPage1.xaml.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using System.Windows.Resources;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using Microsoft.Phone.Storage;
+using System.Collections.ObjectModel;
+using System.IO;
+using Cimbalino.Phone.Toolkit.Extensions;
+using System.Text;
+
+namespace wp8napiv2
+{
+    public partial class PivotPage1 : PhoneApplicationPage
+    {
+        public ObservableCollection<ExternalStorageFile> files;
+        public ObservableCollection<ExternalStorageFolder> folders;
+
+        public ExternalStorageFolder current_folder;
+
+        public static string postData, htmlData, napisy, file_patch;
+
+        public bool testing = true; // BOOL FOR TESTS, NEED TO BE DELETED IN FULL APP
+
+        public PivotPage1()
+        {
+            InitializeComponent();
+        }
+
+        private async void Pivot_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            ExternalStorageDevice _sdCard = (await ExternalStorage.GetExternalStorageDevicesAsync()).FirstOrDefault();
+
+            if (_sdCard != null) loadFolder(_sdCard.RootFolder);
+            else MessageBox.Show("WP8Napi nie znalazł karty SD. Sprawdz czy karta została (poprawnie) włożona i spróbuj jeszcze raz.");
+
+        }
+
+        private async void loadFolder(ExternalStorageFolder fol_tmp)
+        {
+            try
+            {
+                current_folder = fol_tmp;
+
+                folders.Clear();
+                files.Clear();
+
+                IEnumerable<ExternalStorageFile> files_tmp = await fol_tmp.GetFilesAsync();
+                IEnumerable<ExternalStorageFolder> folders_tmp = await fol_tmp.GetFoldersAsync();
+
+                foreach (ExternalStorageFile i in files_tmp)
+                {
+                    files.Add(i);
+                }
+
+                foreach (ExternalStorageFolder i in folders_tmp)
+                {
+                    folders.Add(i);
+                }
+            }
+
+            catch (Exception w) { MessageBox.Show(w.Message); } // Tu sie cos kiedys napisze
+
+
+        }
+
+        private void ListBox_SelectionChanged_folders(object sender, SelectionChangedEventArgs e)
+        {
+            ListBox lb = (ListBox)sender;
+
+            if (lb.SelectedItem != null)
+            {
+                loadFolder((ExternalStorageFolder)lb.SelectedItem);
+            }
+        }
+
+        private void ListBox_SelectionChanged_files(object sender, SelectionChangedEventArgs e)
+        {
+            //ListBox lb = (ListBox)sender; <==== FOR TESTING !!!
+
+            if (//lb.SelectedItem != null <==== FOR TESTING !!!
+                 testing
+                )
+            {
+                //ExternalStorageFile patch = (ExternalStorageFile)lb.SelectedItem; <==== FOR TESTING !!!
+
+                //FileStream file = File.OpenRead(patch.Path); <==== FOR TESTING !!!
+                FileStream file = File.OpenRead("Resources/twd402.mp4");
+
+                byte[] file_byte = new byte[10485760];
+
+                file.Read(file_byte, 0, 10485760);
+
+                byte[] data = file_byte.ComputeMD5Hash();
+
+                StringBuilder sBuilder = new StringBuilder();
+
+                // Loop through each byte of the hashed data  
+                // and format each one as a hexadecimal string. 
+                for (int i = 0; i < data.Length; i++)
+                {
+                    sBuilder.Append(data[i].ToString("x2"));
+                }
+
+                // Return the hexadecimal string. 
+
+                //MessageBox.Show(sBuilder.ToString());
+
+                postData = "mode=1&client=NapiProjektPython&client_ver=0.1&downloaded_subtitles_lang=PL&downloaded_subtitles_txt=1&downloaded_subtitles_id=" + sBuilder.ToString();
+                file_patch = file.Name;
+
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://napiprojekt.pl/api/api-napiprojekt3.php");
+                request.ContentType = "application/x-www-form-urlencoded";
+                request.Method = "POST";
+
+                request.BeginGetRequestStream(new AsyncCallback(GetRequestStreamCallback), request);
+            }
+        }
+
+        private async void Refresh_Click(object sender, RoutedEventArgs e)
+        {
+            ExternalStorageDevice _sdCard = (await ExternalStorage.GetExternalStorageDevicesAsync()).FirstOrDefault();
+
+            if (_sdCard != null) loadFolder(current_folder);
+            else MessageBox.Show("WP8Napi nie znalazł karty SD. Sprawdz czy karta została (poprawnie) włożona i spróbuj jeszcze raz.");
+        }
+
+        public void GetRequestStreamCallback(IAsyncResult asyncResult)
+        {
+            HttpWebRequest request = (HttpWebRequest)asyncResult.AsyncState;
+
+            Stream postStream = request.EndGetRequestStream(asyncResult);
+
+            byte[] byteArray = Encoding.UTF8.GetBytes(postData.ToString());
+
+            postStream.Write(byteArray, 0, postData.Length);
+            postStream.Close();
+
+            request.BeginGetResponse(new AsyncCallback(GetResponseCallback), request);
+        }
+
+        private void GetResponseCallback(IAsyncResult ar)
+        {
+            Deployment.Current.Dispatcher.BeginInvoke(() =>
+            {
+
+                HttpWebRequest request = ar.AsyncState as HttpWebRequest;
+                WebResponse response = request.EndGetResponse(ar);
+
+                StreamReader txt = new StreamReader(response.GetResponseStream(), System.Text.UTF8Encoding.UTF8);
+
+                string str = txt.ReadToEnd();
+
+                txt.Close();
+
+                htmlData = str;
+
+                //MessageBox.Show(str);
+
+                afterRequestCall();
+
+            });
+        }
+
+        private void afterRequestCall()
+        {
+            //MessageBox.Show("2");
+
+            int startIndex = htmlData.IndexOf("<content><![CDATA[") + 18;
+            int endIndex = htmlData.IndexOf("]]></content>", startIndex);
+            string todecode = htmlData.Substring(startIndex, endIndex - startIndex);
+
+            byte[] data = Convert.FromBase64String(todecode);
+            napisy = UTF8Encoding.UTF8.GetString(data, 0, data.Length);
+
+            //MessageBox.Show(napisy);
+
+            NavigationService.Navigate(new Uri("/Page1.xaml?patch=" + file_patch, UriKind.Relative));
+        }
+
+        private void For_tests(object sender, RoutedEventArgs e)
+        {
+            //MessageBox.Show("1");
+
+            ListBox_SelectionChanged_files(null, null);
+        }
+    }
+}
